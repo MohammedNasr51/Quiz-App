dafc86e6f68bc523549e450db0a0e07e8b1bb12c
Mohamed Nasr <mnasr0868@gmail.com>
2025-02-14 01:55:26 +0200

    initially adding condition styles to the selected answer but still a bug that we will solve

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,19 +2,33 @@ import { useState, useCallback } from "react";
 import QUESTIONS from "../utils/questions.js";
 import quizCompletedImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
-const QUESTION_TIME_LIMIT = 15000;
+import { use } from "react";
+const QUESTION_TIME_LIMIT = 10000;
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-  const currentQuestion = userAnswers.length;
+  const [answerState, setAnswerState] = useState("");
+  const currentQuestion =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
   const isQuizCompleted = currentQuestion === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    newAnswer
-  ) {
-    setUserAnswers((prevAnswers) => [...prevAnswers, newAnswer]);
-  },
-  []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(newAnswer) {
+      setAnswerState("answered");
+      setUserAnswers((prevAnswers) => [...prevAnswers, newAnswer]);
+      setTimeout(() => {
+        if (newAnswer === QUESTIONS[currentQuestion].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [currentQuestion]
+  );
 
   const handleSkipAnswer = useCallback(() => {
     handleSelectAnswer(null);
@@ -36,18 +50,34 @@ export default function Quiz() {
     <div id="quiz">
       <div id="question">
         <QuestionTimer
+          key={currentQuestion} // Add key prop to QuestionTimer to recreate it every time the currentQuestion changes
           timeOut={QUESTION_TIME_LIMIT}
           onTimeOut={handleSkipAnswer}
         />
         <h2>{QUESTIONS[currentQuestion].text}</h2>
         <ul id="answers">
-          {shuffledQuestions.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shuffledQuestions.map((answer) => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let answerClass = "";
+            if (answerState === "answered" && isSelected) {
+              answerClass = "selected";
+            }
+
+            if (answerState === ("correct" || "wrong") && isSelected) {
+              answerClass = answerState;
+            }
+
+            return (
+              <li key={answer} className="answer">
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={answerClass}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
